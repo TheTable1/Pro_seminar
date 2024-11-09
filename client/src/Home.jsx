3f36018c09df5b88ba8110b95869344df0d13d2b
F_F <florence.f@kkumail.com>
2024-11-09 21:51:31 +0700

    history,home(ยังไม่เสร็จ)

---
+++ client/src/Home.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useRef } from "react";
+import L from "leaflet";
+import "./assets/css/country.css";
+import "leaflet/dist/leaflet.css";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default Home;
