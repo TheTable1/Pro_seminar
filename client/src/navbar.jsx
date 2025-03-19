89e8d5fd38fce729f1d219160d7b24c3a970f560
“Florence <162617498+TheTable1@users.noreply.github.com>
2025-03-19 21:19:43 +0700

    Update navbar.jsx

--- client/src/navbar.jsx
+++ client/src/navbar.jsx
@@ -329,6 +329,11 @@ export default function Navbar() {
                   แบบทดสอบ
                 </Link>
               </li>
+              <li className="hover:bg-brown pt-3 rounded transition duration-200 cursor-pointer">
+                <Link to="/quiz" onClick={() => setShowMenu(false)}>
+                  โปรไฟล์
+                </Link>
+              </li>
             </ul>
           </div>
         )}
